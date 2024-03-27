8bca18e0385a9f023b1016d495d4108687e373b5
João Trindade <joao.trindade@isel.pt>
2024-03-27 09:20:20 +0000

    Lecture 13 - FixedPool

---
+++ src/main/kotlin/lecture13/executors/FixedPool.kt
@@ -0,0 +1,29 @@
+package pt.isel.pc.jht.executors.fixed
+
+import java.util.concurrent.*
+
+val executor = Executors.newFixedThreadPool(
+	Runtime.getRuntime().availableProcessors()
+)
+
+fun main() {
+
+	val mainTid = Thread.currentThread().id
+	println("[T$mainTid] :: STARTING ::")
+
+	for (n in 0..15) {
+		executor.execute {
+			val tid = Thread.currentThread().id
+			println("[T$tid] executing task #$n")		
+		}
+	}
+
+	println("[T$mainTid] :: WAITING ::")
+	executor.shutdown()
+
+	if (!executor.awaitTermination(3, TimeUnit.SECONDS)) {
+		println("[T$mainTid] :: timed out ::")
+	} else {
+		println("[T$mainTid] :: DONE ::")
+	}
+}
